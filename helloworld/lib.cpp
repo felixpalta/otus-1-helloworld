ba1be80ecd79a6e33c244f5969127225b90517cd
Felix Palta <fpalta90@gmail.com>
2019-07-05 00:40:40 +0300

    First commit

---
+++ helloworld/lib.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+
+#include "version.h"
+
+namespace otus {
+
+int GetVersion() {
+    return APP_PATCH_VERSION;
+}
+
+} // otus
---
+++ helloworld/main.cpp
@@ -0,0 +1,9 @@
+#include <lib.h>
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello world!\n"
+              << "Version: " << GetVersion() << std::endl;
+    return 0;
+}
