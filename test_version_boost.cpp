617895115f28e80b19afa3a5bc8fb199e2cbe386
Felix Palta (office) <fpalta90@gmail.com>
2019-07-05 17:52:05 +0300

    Fix test

--- test_version_boost.cpp
+++ test_version_boost.cpp
@@ -3,11 +3,11 @@
 #include "lib.hpp"
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(test_version_boost)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(otus::GetVersion() == 0);
+    BOOST_CHECK(otus::GetVersion() > 0);
 }
 
 }
